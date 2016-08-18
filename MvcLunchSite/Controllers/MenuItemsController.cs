bbf505f35b9657a64cdefb513a38386dfdee4864
Tyler Herrick <therrick47@ymail.com>
2016-08-18 11:04:15 -0700

    my version
    added my version to test out a couple of parts from jordan's copy

---
+++ MvcLunchSite/Controllers/MenuItemsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcLunchSite.Models;
+
+namespace MvcLunchSite.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MenuItems
+        public ActionResult Index()
+        {
+            var menuItems = db.MenuItems.Include(m => m.Menu);
+            return View(menuItems.ToList());
+        }
+
+        // GET: MenuItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Create
+        public ActionResult Create()
+        {
+            ViewBag.menuID = new SelectList(db.Menus, "menuID", "menuName");
+            return View();
+        }
+
+        // POST: MenuItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "menuItemID,menuItemName,menuItemPrice,menuItemDescription,menuID")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MenuItems.Add(menuItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.menuID = new SelectList(db.Menus, "menuID", "menuName", menuItem.menuID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.menuID = new SelectList(db.Menus, "menuID", "menuName", menuItem.menuID);
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "menuItemID,menuItemName,menuItemPrice,menuItemDescription,menuID")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menuItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.menuID = new SelectList(db.Menus, "menuID", "menuName", menuItem.menuID);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MenuItem menuItem = db.MenuItems.Find(id);
+            db.MenuItems.Remove(menuItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcLunchSite/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLunchSite.Models
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public ICollection<Menu> Menus { get; set; }
+        public List<Restaurant> GetRestaurants()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<Restaurant> restaurants = new List<Restaurant>();
+            foreach (Restaurant r in db.Restaurants)
+                restaurants.Add(r);
+            return restaurants;
+        }
+    }
+}
