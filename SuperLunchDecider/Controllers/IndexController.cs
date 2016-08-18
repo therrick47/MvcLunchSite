b4a1a2b2e315edcceb5eba2660b494e55272dfdf
Tyler Herrick <therrick47@ymail.com>
2016-08-18 08:03:35 -0700

    Added Site Project
    Added Mvc solution so that it can be worked on anywhere

---
+++ SuperLunchDecider/Controllers/IndexController.cs
@@ -0,0 +1,30 @@
+﻿using SuperLunchDecider.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperLunchDecider.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Restaurants = GetRestaurants();
+            return View(mymodel);
+        }
+        public List<Restaurant> GetRestaurants()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<Restaurant> restaurants = new List<Restaurant>();
+            foreach (Restaurant r in db.Restaurants)
+                restaurants.Add(r);
+            return restaurants;
+        }
+    }
+}
