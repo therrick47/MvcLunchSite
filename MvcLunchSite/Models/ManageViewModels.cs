d31e204bf4c1294004df177db4db9ad7a9011d18
jumbochoco <jordanfoxnews@gmail.com>
2016-10-13 16:03:41 -0700

    This is broken. Implementing user roles.

--- MvcLunchSite/Models/ManageViewModels.cs
+++ MvcLunchSite/Models/ManageViewModels.cs
@@ -9,9 +9,8 @@ namespace MvcLunchSite.Models
     {
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public string RoleName { get; set; }
     }
 
     public class ManageLoginsViewModel
@@ -56,31 +55,5 @@ namespace MvcLunchSite.Models
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-    }
-
-    public class AddPhoneNumberViewModel
-    {
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string Number { get; set; }
-    }
-
-    public class VerifyPhoneNumberViewModel
-    {
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-    }
-
-    public class ConfigureTwoFactorViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
     }
 }
