926aa3c387a7e6fd7cae91ca10265c1d836c591a
Tyler Herrick <therrick47@ymail.com>
2016-10-15 01:36:02 -0700

    Merge remote-tracking branch 'origin/Tyler's-Version' into Tyler's-Version
    Not really sure what github is doing so Jordan's commit may have
    disappeared. I saw it on the website but not desktop client. But I made
    sure to keep the changes (didn't gut anything this time) to try and fix
    the issue. Accounts are now part of the AspNetUsers table. The Manage
    page displays user role to confirm that the data is getting passed
    around. Next step is to make page viewing restrictions based on user
    role.

--- MvcLunchSite/App_Start/IdentityConfig.cs
+++ MvcLunchSite/App_Start/IdentityConfig.cs
@@ -67,15 +67,6 @@ namespace MvcLunchSite
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
             // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
-            {
-                MessageFormat = "Your security code is {0}"
-            });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
-            {
-                Subject = "Security Code",
-                BodyFormat = "Your security code is {0}"
-            });
             manager.EmailService = new EmailService();
             manager.SmsService = new SmsService();
             var dataProtectionProvider = options.DataProtectionProvider;
