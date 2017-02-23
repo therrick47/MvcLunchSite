e0a7a5973fec970040fb8c1ac6300b6f6d52bb98
Tyler Herrick <therrick47@ymail.com>
2017-02-23 12:27:15 -0700

    dropdownlist on ordering page now updates display with restaurants based on the selected restaurant

--- MvcLunchSite/Controllers/OrdersController.cs
+++ MvcLunchSite/Controllers/OrdersController.cs
@@ -10,9 +10,46 @@ namespace MvcLunchSite.Controllers
     public class OrdersController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+        public List<string> getTopScores()
+        {
+            Dictionary<int, int> topScores = new Dictionary<int, int>();
+            foreach (var item in db.Restaurants)
+            {
+                topScores.Add(item.ID, 0);
+            }
+            foreach (var user in db.Users)
+            {
+                if (user.FirstChoice != null && topScores.ContainsKey(user.FirstChoice.GetValueOrDefault()))
+                {
+                    topScores[user.FirstChoice.GetValueOrDefault()] += 3;
+                }
+                if (user.SecondChoice != null && topScores.ContainsKey(user.SecondChoice.GetValueOrDefault()))
+                {
+                    topScores[user.SecondChoice.GetValueOrDefault()] += 2;
+                }
+                if (user.ThirdChoice != null && topScores.ContainsKey(user.ThirdChoice.GetValueOrDefault()))
+                {
+                    topScores[user.ThirdChoice.GetValueOrDefault()] += 1;
+                }
+            }
+            topScores = topScores.OrderByDescending(pair => pair.Value).Take(5).ToDictionary(pair => pair.Key, pair => pair.Value);
+            List<string> topScoresOutputList = new List<string>();
+            foreach (var rest in topScores)
+            {
+                var query = from item in db.Restaurants
+                            where item.ID.Equals(rest.Key)
+                            select item;
+                var queryItem = query.FirstOrDefault();
+                string finalString = queryItem.name + ": " + rest.Value.ToString();
+                topScoresOutputList.Add(finalString);
+            }
+            return topScoresOutputList;
+        }
+
         // GET: Orders
         public ActionResult Index()
         {
+            ViewData["TopScoresList"] = getTopScores();
             ViewData["UserList"] = db.Users.ToList();
             ViewData["OrderList"] = db.Orders.ToList();
             ViewData["RestaurantList"] = db.Restaurants.ToList();
@@ -24,5 +61,11 @@ namespace MvcLunchSite.Controllers
             ViewBag.orderID = new SelectList(db.Orders, "orderID", "menuItemID", RouteData.Values["id"]);
             return View();
         }
+        /*public ActionResult Print()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Print()*/
     }
 }
